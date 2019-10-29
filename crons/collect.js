6048220284b338b6d3d20156bcecbad0b5cd3c6c
Benjamin J. Young <blaher09@sbcglobal.net>
2019-10-29 16:18:18 -0400

    Fixing bug where last minute not filling due to seconds over threshold

--- crons/collect.js
+++ crons/collect.js
@@ -29,6 +29,14 @@ function get_max_minute() {
   return moment('16:00:00', 'HH:mm:ss');
 }
 
+function get_lower_minute() {
+  return get_min_minute();
+}
+
+function get_upper_minute() {
+  return get_min_minute().add(1, 'minutes');
+}
+
 function get_previous_minute(minute) {
   //TODO: Consider Weekends and Holidays too
   const minute_moment = get_minute_moment(minute);
@@ -305,10 +313,10 @@ function load_init(models) {
   //TODO: Check market is open with Alpaca
   var minute = get_minute();
   const current_minute = get_current_minute();
-  const min_minute = get_min_minute();
-  const max_minute = get_max_minute();
+  const lower_minute = get_lower_minute();
+  const upper_minute = get_upper_minute();
 
-  if (current_minute.isSameOrBefore(max_minute) && current_minute.isSameOrAfter(min_minute)) {
+  if (current_minute.isBefore(upper_minute) && current_minute.isAfter(lower_minute)) {
     console.log('Starting Collect...');
 
     loop_through(minute, models);
