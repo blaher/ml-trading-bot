f105246b61c2f84c403fc0f5698a2afa042e0795
Benjamin J. Young <blaher09@sbcglobal.net>
2019-11-03 05:48:50 -0500

    Adding comparisons to testing suite

--- crons/test.js
+++ crons/test.js
@@ -10,6 +10,8 @@ const sequelize = require('sequelize');
 const child_process = require('child_process');
 
 router.get('/', function(req, res) {
+  //TODO: Add query parameter to back test against end date
+
   models.Indexes.findAll({
     include: [models.Indicators, models.Stocks]
   }).then(function(indexes) {
@@ -51,6 +53,10 @@ router.get('/', function(req, res) {
       }).then(function(rows) {
         const starting_value = 30000;
         var value = starting_value;
+        var index_value = value;
+        var perfect_value = value;
+        var missed_value = value;
+        var bad_value = value;
 
         rows.forEach(function(row, i) {
           columns.forEach(function(column) {
@@ -58,11 +64,8 @@ router.get('/', function(req, res) {
           });
         });
 
-        //TODO: Figure out why 1 is coming before 0
-        //TODO: Find why first two are NaN
-        //TODO: Show Index comparios
-        //TODO: Show what profit was missed on
-        //TODO: Show what profit was lost
+        //TODO: Figure out why 1 is coming before 0 sometimes
+        //TODO: Find why first two are NaN sometimes
         var wait_time = 0;
         rows.forEach(function(row, i) {
           wait_time += 2000;
@@ -113,22 +116,47 @@ router.get('/', function(req, res) {
               console.log('Neural Prediction: '+neural);
               console.log('Stock Price: '+current_stock_price);
               console.log('Future Stock Price: '+future_stock_price);
+              console.log('');
+
+              const index_qty = Math.floor((index_value*4)/current_stock_price);
+              const index_purchase_amount = Math.ceil(index_qty*(current_stock_price*100))/100;
+              const index_sell_amount = Math.floor(index_qty*(future_stock_price*100))/100;
+              const index_profit = ((index_sell_amount*100)-(index_purchase_amount*100))/100;
+              index_value = ((index_value*100)+(index_profit*100))/100;
+
+              const qty = Math.floor((value*4*0.9)/current_stock_price);
+              const purchase_amount = Math.ceil(qty*(current_stock_price*100))/100;
+              const sell_amount = Math.floor(qty*(future_stock_price*100))/100;
+              const profit = ((sell_amount*100)-(purchase_amount*100))/100;
 
               if (guess === 1) {
-                const qty = Math.floor((value*4*0.9)/current_stock_price);
-                const purchase_amount = Math.ceil(qty*(current_stock_price*100))/100;
-                const sell_amount = Math.floor(qty*(future_stock_price*100))/100;
-                const profit = ((sell_amount*100)-(purchase_amount*100))/100;
-
                 console.log('Buying...');
                 console.log('Quantity: '+qty);
                 console.log('Purchase Amount: '+purchase_amount);
                 console.log('Sell Amount: '+sell_amount);
                 console.log('Profit: '+profit);
 
+                if (profit > 0) {
+                  perfect_value = ((perfect_value*100)+(profit*100))/100;
+                } else {
+                  bad_value = ((bad_value*100)+(profit*100))/100;
+                }
+
                 value = ((value*100)+(profit*100))/100;
+              } else {
+                console.log('Selling...');
+
+                if (profit > 0) {
+                  perfect_value = ((perfect_value*100)+(profit*100))/100;
+                  missed_value = ((missed_value*100)+(profit*100))/100;
+                }
               }
 
+              console.log('');
+              console.log('Index Value: $'+index_value);
+              console.log('Missed Value: $'+missed_value);
+              console.log('Bad Value: $'+bad_value);
+              console.log('Perfect Value: $'+perfect_value);
               console.log('Value: $'+value);
             });
 
@@ -136,6 +164,8 @@ router.get('/', function(req, res) {
             py.stdin.end();
           }, wait_time);
         });
+
+        //TODO: Add some final info like accuracy, index beat percentages
       });
     });
   });
